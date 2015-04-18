0d1c67fd9d762d79301fcdaf671bf31d435467a9
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-18 18:00:23 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,17 @@
+This is a test using the PN532 NFC / RFID controller with Arduino
+
+This example uses the library from Adafruit: Adafruit_PN532
+
+“This is a library for the Adafruit PN532 NFC/RFID breakout boards
+This library works with the Adafruit NFC breakout 
+  ----> https://www.adafruit.com/products/364
+ 
+Check out the links above for our tutorials and wiring diagrams 
+These chips use I2C or SPI to communicate.
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada & Kevin Townsend for Adafruit Industries.  
+BSD license, check license.txt for more information”
---
+++ rfidTest.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+// Use Arduino default SPI connection
+// SCK = 13, MOSI = 11, MISO = 12, SS = 10
+#define SS_PIN   (10)
+Adafruit_PN532 nfc(SS_PIN);
+
+void setup(void) {
+  Serial.begin(115200);
+  setupReader();
+}
+
+void setupReader(void)
+{
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find RFID reader");
+    while (1); // halt
+  }
+  Serial.println("Found RFID reader");
+  nfc.SAMConfig();
+  Serial.println("Waiting for card");
+}
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID//
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+    Serial.print("\n{\n \"UID\" : ");
+    nfc.PrintHex(uid, uidLength);
+    Serial.print("}\n");
+  }
+}
